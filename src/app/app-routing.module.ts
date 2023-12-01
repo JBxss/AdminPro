e755e71af61690f8708d60c37aa3ab109ab35407
Juan Sebastian Bossa Alvarez <sistemas78@red5g.co>
2023-12-01 10:14:17 -0500

    [ADD] Implementacion de Rutas Secundarias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,19 +6,28 @@ import { RegisterComponent } from './auth/register/register.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
-  { path: "dashboard", component: DashboardComponent },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
-  { path: "progress", component: ProgressComponent },
-  { path: "grafica1", component: Grafica1Component },
-  { path: "", redirectTo: "/dashboard", pathMatch: "full"},
-  { path: "**", component: NopagefoundComponent}
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: "", redirectTo: "/dashboard", pathMatch: "full"},
+    ],
+  },
+
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+
+  { path: '**', component: NopagefoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
