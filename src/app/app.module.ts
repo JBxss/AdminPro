328067a2e3fea4b8c7a785d63be59493baafa26f
Juan Sebastian Bossa Alvarez <sistemas78@red5g.co>
2023-12-01 15:23:40 -0500

    [UPDATE] Separacion de Logica por Modulos V2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,11 @@ import { PagesModule } from './pages/pages.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NopagefoundComponent,
     
   ],
