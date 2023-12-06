8acaa5dfffab680e6c2ceb2f994dfcd5db423295
Juan Sebastian Bossa Alvarez <sistemas78@red5g.co>
2023-12-06 16:16:29 -0500

    [UPDATE] Mejoras en funcionalidades

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -3,17 +3,16 @@ import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
-  styles: ``
+  styles: ``,
 })
 export class IncrementadorComponent implements OnInit {
-
   ngOnInit() {
     this.btnClass = `btn ${this.btnClass}`;
   }
-    // @Input() progreso: number = 90;
+  // @Input() progreso: number = 90;
   @Input('valor') progreso: number = 90;
   @Input() btnClass: string = 'btn-primary';
-  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter;
+  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
 
   cambiarValor(valor: number) {
     if (this.progreso >= 100 && valor >= 0) {
@@ -27,6 +26,18 @@ export class IncrementadorComponent implements OnInit {
     }
 
     this.progreso = this.progreso + valor;
+    this.valorSalida.emit(this.progreso);
+  }
+
+  onChange(nuevoValor: number) {
+    if (nuevoValor >= 100) {
+      this.progreso = 100;
+    } else if (nuevoValor <= 0) {
+      this.progreso = 0;
+    } else {
+      this.progreso = nuevoValor;
+    }
+
     this.valorSalida.emit(this.progreso);
   }
 }
