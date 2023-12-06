3b00118fde4f1a3602797a21e982473bd8e194ab
Juan Sebastian Bossa Alvarez <sistemas78@red5g.co>
2023-12-06 14:59:17 -0500

    [UPDATE] Componente incrementar mediante Input y Output

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -6,21 +6,22 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class IncrementadorComponent {
-  progreso: number = 90;
-
-  get getPorcentaje() {
-    return `${this.progreso}%`;
-  }
+    // @Input() progreso: number = 90;
+  @Input('valor') progreso: number = 90;
+  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter;
 
   cambiarValor(valor: number) {
     if (this.progreso >= 100 && valor >= 0) {
+      this.valorSalida.emit(100);
       return (this.progreso = 100);
     }
 
     if (this.progreso <= 0 && valor < 0) {
+      this.valorSalida.emit(0);
       return (this.progreso = 0);
     }
 
     this.progreso = this.progreso + valor;
+    this.valorSalida.emit(this.progreso);
   }
 }
