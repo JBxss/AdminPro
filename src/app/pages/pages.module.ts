1d5d08d6ece3dccc93612fd5e592e476d90cd4f3
Juan Sebastian Bossa Alvarez <sistemas78@red5g.co>
2023-12-06 11:16:07 -0500

    [FEATURE] Funcionalidades y estilos del ProgressBar hechas

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,6 +6,7 @@ import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,6 @@ import { RouterModule } from '@angular/router';
     Grafica1Component,
     PagesComponent,
   ],
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [CommonModule, SharedModule, RouterModule, FormsModule],
 })
 export class PagesModule {}
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: ``
+  styleUrls: ['./progress.component.css'],
 })
 export class ProgressComponent {
+  progreso: number = 90;
 
+  get getPorcentaje() {
+    return `${this.progreso}%`;
+  }
+
+  cambiarValor(valor: number) {
+    if (this.progreso >= 100 && valor >= 0) {
+      return (this.progreso = 100);
+    }
+
+    if (this.progreso <= 0 && valor < 0) {
+      return (this.progreso = 0);
+    }
+
+    this.progreso = this.progreso + valor;
+  }
 }
